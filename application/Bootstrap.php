79e50d6af5f92474ccca86dd111f4173c58f0785
Thomaz Takashi Oda Toyama <contato@thomazot.com.br>
2011-11-25 11:01:48 -0200

    teste

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -8,6 +8,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     protected function _initConfig() {
         $config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/application.ini', APPLICATION_ENV);
         Zend_Registry::set('config', $config);
+        
     }
     
     /**
